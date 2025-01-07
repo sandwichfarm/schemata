5bb6db9d98b428727ab27c16056a12abf93f6cbe
Sandwich <299465+dskvr@users.noreply.github.com>
2025-01-07 08:56:53 +0700

    further simplify aliases

--- scripts/deref.js
+++ scripts/deref.js
@@ -10,9 +10,7 @@ const output = args[1];
 try {
     const schemaData = await fs.readFile(path.resolve(input), 'utf-8');
     const schema = JSON.parse(schemaData);
-    
     const dereferencedSchema = await $RefParser.dereference(schema);
-    
     await fs.writeFile(output, JSON.stringify(dereferencedSchema, null, 2), 'utf-8');
     console.log(`Schema written to ${output}`);
 } catch (err) {
